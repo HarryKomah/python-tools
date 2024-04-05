9131f8a7821d2ef82b522241f3a40562a3ee87f5
HarryKomah <harrykomah2003@gmail.com>
2024-04-06 00:36:15 +0800

    adding decorators

--- time/time_measure.py
+++ time/time_measure.py
@@ -1,5 +1,4 @@
 import time
-
 """
 Basic skeleton:
 
@@ -14,6 +13,12 @@ def measure_time(func, *args):
     return time_elapsed
 
 """
+
+def stopwatch_decorator(f):
+    def wrapper(*args, **kwargs):
+        measure_time(f, *args, **kwargs)
+    return wrapper
+
 def measure_time(func, *args, cpu=False):
     """
     Measure the execution time of a function.
@@ -25,21 +30,25 @@ def measure_time(func, *args, cpu=False):
 
     Returns:
         None
-    """
-  start_time = time.process_time() if cpu else time.time()
-  if args:
-      func(*args) # star operator will unpack the arguments.
-  else:
-      func()
-  end_time = time.process_time() if cpu else time.time()
-  elapsed = end_time - start_time
-  seconds = str(elapsed) + " CPU cycle." if cpu else " seconds."
-  print(func.__name__ + " executed and finished in " + str(elapsed) + seconds)
+"""
+    start_time = time.process_time() if cpu else time.time()
+    if args:
+        func(*args) # star operator will unpack the arguments.
+    else:
+        func()
+    end_time = time.process_time() if cpu else time.time()
+    elapsed = end_time - start_time
+    seconds = str(elapsed) + " CPU cycle." if cpu else " seconds."
+    print(func.__name__ + " executed and finished in " + str(elapsed) + seconds)
 
 # EXAMPLE usage:
 
+
+@stopwatch_decorator
 def hello_world():
     print("Hello World")
     return b"Hello World"
 
-measure_time(hello_world, cpu=True)
+#measure_time(hello_world, cpu=True)
+
+hello_world()
